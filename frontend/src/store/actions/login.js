9a84a0b6865ec47680bed651c7090c117a7ae3e2
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-01-10 22:41:38 +0530

    login action corrected, redux implemented in the code(1)

--- frontend/src/store/actions/login.js
+++ frontend/src/store/actions/login.js
@@ -15,32 +15,30 @@ export const login = () => {
 }
 
 export const logout = async() => {
-    try {
-        const cookies = document.cookie.split('; ');
-        const value = cookies.find(item => item.startsWith('jwt')).split('=')[1];
-        const response = await axios({
-            url: baseUrl + '/auth/logout',
-            method: 'post',
-            headers: {
-            Authorization: `Bearer ${value}`
-            }
-        })
+    return (dispatch) => {
+        try {
+            const cookies = document.cookie.split('; ');
+            const value = cookies.find(item => item.startsWith('jwt')).split('=')[1];
+            const response = await axios({
+                url: baseUrl + '/auth/logout',
+                method: 'post',
+                headers: {
+                Authorization: `Bearer ${value}`
+                }
+            })
 
-        if(response.status === 200) {
-            document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-            alert("you have been logged out");
-            return (dispatch) => {
+            if(response.status === 200) {
+                document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                alert("you have been logged out");
                 dispatch({ type: actionTypes.LOGOUT });
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
             }
-        } else {
-            var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            error.response = response;
-            throw error;
-        }
-    } catch(error) { 
-        alert("Could not logout.\nError: "+ error.message);
-        return (dispatch) => {
+        } catch(error) { 
+            alert("Could not logout.\nError: "+ error.message);
             dispatch({ type: actionTypes.LOGIN });
-        }
-    }; 
+        }; 
+    };
 }
