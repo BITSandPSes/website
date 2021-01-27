3bec7c8730aff7cc7fbc8e9297ceec2c39a0c880
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2021-01-27 19:38:39 +0000

    common search bar

---
+++ frontend/src/containers/Home/search/search.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Select, {components} from 'react-select';
+import { Redirect } from 'react-router'
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      options: this.props.options,
+      loc: this.props.loc,
+      redirect: null,
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      redirect: e.value
+    })
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return(
+        <Redirect to = {this.state.loc + this.state.redirect}/>
+      )
+    }
+
+    return(
+      <Select
+        onChange={this.handleChange}
+        options={this.state.options}
+      />
+    )
+  }
+}
+
+export default Search;
