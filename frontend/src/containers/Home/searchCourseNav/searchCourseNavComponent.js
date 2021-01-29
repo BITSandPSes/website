d2eb3209f9d404467b6f7edc3f2ce1bcf7536445
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2021-01-29 11:44:07 +0000

    (temporary fix) course page search bar

--- frontend/src/containers/Home/searchCourseNav/searchCourseNavComponent.js
+++ frontend/src/containers/Home/searchCourseNav/searchCourseNavComponent.js
@@ -1,51 +1,42 @@
 import React, { Component } from 'react';
-import { Button, Row } from 'reactstrap';
 import "./searchCourseNav.css";
-import { Redirect } from 'react-router-dom';
-import { LocalForm , Control } from 'react-redux-form';
-
-const minLength = (len) => (val) => val && (val.length >= len);
+import Search from '../search/search'
+import axios from 'axios';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      router: false,
-      searchField: null
-    };
-    this.preventSearch = this.preventSearch.bind(this);
+      options: []
+    }
   }
 
-  preventSearch = (values) => {
-    this.setState({
-      router: true,
-      searchField: values.search
-    });
+  async componentDidMount() {
+    try {
+      const { data } = await axios('/api/course');
+
+      const options = [];
+      data.forEach(course => {
+        options.push({
+          value: course.slug,
+          label: course.number + ' ' + course.title
+        })
+      });
+
+      this.setState({
+        options: options
+      });
+    } catch (error) {
+      alert("could not fetch search results.\nError: " + error.message);
+    }
   }
-  
+
   render() {
-    return(
-      <div className = "row row-search align-items-center fixed-top envelope zerk">
-        <div className = "col-10 offset-1 col-md-8 offset-md-2 justify-content-center">
-          <LocalForm onSubmit = { (values) => { this.preventSearch(values) } } autoComplete = "off" >
-            <Row className = "form-group">
-              <div className = "col-9 col-md-8 offset-md-1">
-                <Control.text
-                model = ".search"
-                name = "search"
-                id = "search"
-                placeholder = "Search for course"
-                className = "col-12 form-control search-nav"
-                validators = { { minLength: minLength(1)} } />
-              </div>
-              <Button 
-               type = "submit" 
-               className = "btn col-3 col-md-2 align-self-start search-nav-button" >Search</Button>
-            </Row>
-          </LocalForm>
-          { this.state.router ? 
-            <Redirect to = { "/3/home?Search=" + this.state.searchField + "&searchMethod=title" } />
-          : null}
+    return (
+      <div className="row row-search align-items-center fixed-top envelope zerk">
+        <div className="col-10 offset-1 col-md-8 offset-md-2 justify-content-center">
+          <Search key={this.state.options} options={this.state.options} loc={'/3/course/'} />
         </div>
       </div>
     );
