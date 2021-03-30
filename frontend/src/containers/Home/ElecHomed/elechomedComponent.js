a94013073cab8f8a55d84b7ac7c95982889a9f5b
pratham <pratham1002@gmail.com>
2021-03-30 23:30:31 +0530

    minor fix: ui

--- frontend/src/containers/Home/ElecHomed/elechomedComponent.js
+++ frontend/src/containers/Home/ElecHomed/elechomedComponent.js
@@ -242,14 +242,12 @@ class CourseDisplay extends Component {
                   renderResults={results => (
                     <div>
                       {results.map(item => (
-                        <li>
                           <Link className="station-linker" to={'/' + window.localStorage.getItem("stationNo") + '/course/' + item.slug} >
                             <div className="my-3 mx-2 station-links">
                               <h5 className={"text-left elec-station-link-header"}>{item.title}</h5>
                               <h6 className="location-station text-left">{"Number: " + item.number}</h6>
                             </div>
                           </Link>
-                        </li>
                       ))}
                     </div>
                   )}
